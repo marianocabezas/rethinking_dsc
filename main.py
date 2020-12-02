b7e76704babc318dd58980549fc4216312ab014c
mariano <mcabezas@eia.udg.edu>
2020-12-03 10:54:04 +1100

    - added LIT

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def parse_inputs():
             '/home/mariano/data/DiceProject/longitudinal',
             '/home/mariano/data/DiceProject/wmh',
             '/home/mariano/data/DiceProject/msseg',
+            '/home/mariano/data/DiceProject/lit',
             '/home/mariano/data/DiceProject/cross-sectional',
         ],
         help='Option to define the folders for each ask with all the patients.'

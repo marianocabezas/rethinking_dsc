9180f1b7bb9d7d9aa83c3d717f7f8c61c3c32d43
mariano <mcabezas@eia.udg.edu>
2021-02-24 18:53:03 +1100

    - changed data order

--- statistics.py
+++ statistics.py
@@ -154,7 +154,7 @@ def analyse_lesions(d_path, verbose=0):
         y_flat = y.flatten(1)
         b_flat = brain.flatten(1)
 
-        print(torch.sum(y, dim=1), torch.sum(brain, dim=1))
+        print(torch.sum(y), torch.sum(brain))
 
         batch_voxels += torch.sum(y)
         batch_brain_voxels += torch.sum(brain)
